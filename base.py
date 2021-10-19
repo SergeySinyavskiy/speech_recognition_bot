2c3c54c6098dd9fafa75ec98050f8a8193de7ee8
SergeySinyavskiy <55625233+SergeySinyavskiy@users.noreply.github.com>
2021-10-19 16:26:49 +0300

    Create base.py

---
+++ base.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+class Base:
+	def __init__(self, db_config_file):
+		self.config = db_config_file
+
+	def connect(self):
+		self.conn = mysql.connector.connect(option_files=self.config)
+		self.cursor = self.conn.cursor()
+		
+	def add_user(self, user_id):
+		
+	def remove_user(self, user_id):
+		
+	def get_user_language(self, user_id):
+		
+	def change_user_language(self, user_id, lang):
+		
+	def get_users_list(self):
+		
+		
+	#todo: implement methods
