c06ec6c4c9c3d63c4db770385d41abb11f7607e2
serg31Aligator <serg31Aligator@gmail.com>
2021-10-20 16:30:10 +0300

    Added comments

--- bot.py
+++ bot.py
@@ -12,43 +12,42 @@ LANGS = {'En': 'en-US',
 
 user_data = {}
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start']) ### Start script of bot that reads user id and add it to the database
 def get_username(message):
 	uname = message.chat.username
-	if uname not in user_data:
-		user_data[uname] = 'ru-RU'
+	if uname not in user_data: 
+		user_data[uname] = 'ru-RU' ### Default lang parameter for bot translations if userid is new for bot and wasn't met yet
 		
-@bot.message_handler(commands=['stop'])
+@bot.message_handler(commands=['stop']) ### Stop script for bot that deletes user id of current user who working with bot from database
 def remove_username(message):
 	try:
 		del user_data[message.chat.username]
 	except KeyError:
 		print("Username does not exist.")
 
-@bot.message_handler(content_types=['voice'])
+@bot.message_handler(content_types=['voice']) ### Bot script that reads the content of a message. Downloading it and convert it into text representation.
 def process_voice(message):
-	r = sr.Recognizer()
-	file_info = bot.get_file(message.voice.file_id)
-	downloaded_file = bot.download_file(file_info.file_path)
+	rec = sr.Recognizer()
+	file_info = bot.get_file(message.voice.file_id) ### download audio file
+	downloaded_file = bot.download_file(file_info.file_path) ### Open file and convert it in ogg format
 	with open('voice.ogg', 'wb') as new_file:
 		new_file.write(downloaded_file)
-	process = sp.run(['ffmpeg', '-y', '-loglevel', 'quiet', '-i', 'voice.ogg', 'voice.wav'])
+	process = sp.run(['ffmpeg', '-y', '-loglevel', 'quiet', '-i', 'voice.ogg', 'voice.wav']) ### conver process
 	if process.returncode != 0:
 		raise Exception('Something went wrong with audio convertion.')
 	with sr.AudioFile('voice.wav') as source:
-		audio = r.listen(source)
-	text = r.recognize_google(audio, language=user_data[message.chat.username])
-	#print(text)
-	bot.reply_to(message, text)
+		audio = rec.listen(source) ### Parse audio file
+	text = rec.recognize_google(audio, language=user_data[message.chat.username]) ### Translate audio file in accordance with lang parameter
+	bot.reply_to(message, text) ### Bot reply to user 
 
-@bot.message_handler(commands=['change_lang'])
+@bot.message_handler(commands=['change_lang']) ### Change language buttons menu
 def change_language(message):
 	keyboard = types.InlineKeyboardMarkup()
-	key_en = types.InlineKeyboardButton(text='En', callback_data="En")
+	key_en = types.InlineKeyboardButton(text='EN', callback_data="EN")
 	keyboard.add(key_en)
-	key_ru = types.InlineKeyboardButton(text='Ru', callback_data="Ru")
+	key_ru = types.InlineKeyboardButton(text='RU', callback_data="RU")
 	keyboard.add(key_ru)
-	key_ua = types.InlineKeyboardButton(text='Ua', callback_data="Ua")
+	key_ua = types.InlineKeyboardButton(text='UA', callback_data="UA")
 	keyboard.add(key_ua)
 	msg_text = 'Выберите язык сообщения:'
 	bot.send_message(message.from_user.id, text=msg_text, reply_markup=keyboard)
